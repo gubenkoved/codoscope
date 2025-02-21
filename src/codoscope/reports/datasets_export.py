459a2fc937d433d78bc387491fba32790459d15e
Eugene Gubenkov <gubenkoved@gmail.com>
2025-02-21 21:26:15 +0100

    adding ability to export datasets in ndjson format

--- src/codoscope/reports/datasets_export.py
+++ src/codoscope/reports/datasets_export.py
@@ -3,8 +3,9 @@ import os
 import os.path
 
 from codoscope.common import ensure_dir
-from codoscope.config import read_mandatory
+from codoscope.config import read_mandatory, read_optional
 from codoscope.datasets import Datasets
+from codoscope.exceptions import ConfigError
 from codoscope.reports.common import ReportBase, ReportType
 from codoscope.state import StateModel
 
@@ -20,6 +21,16 @@ class DatasetsExportReport(ReportBase):
         out_dir = os.path.abspath(read_mandatory(config, "out-dir"))
         ensure_dir(out_dir)
 
+        data_format = read_optional(config, "format", default="csv")
+
         for name, df in datasets.get_all_data_frames().items():
-            out_path = os.path.join(out_dir, "%s.csv" % name)
-            df.to_csv(out_path, index=False)
+            if data_format == "csv":
+                out_path = os.path.join(out_dir, "%s.csv" % name)
+                df.to_csv(out_path, index=False)
+            elif data_format in ("ndjson", "jsonl"):
+                out_path = os.path.join(out_dir, "%s.jsonl" % name)
+                df.to_json(out_path, orient="records", lines=True)
+            else:
+                raise ConfigError(
+                    f'Unknown data format "{data_format}" supported: csv, ndjson (jsonl)'
+                )
